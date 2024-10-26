5df529239591a8e23cad446bc77d30a971b91187
irfiacre <irfiacre@gmail.com>
2024-10-26 15:39:46 +0200

    Improve project setup & Add user mgt endpoints

--- threadflow/api/models.py
+++ threadflow/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstName=models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    userName = models.CharField(unique= True, max_length=50)
+    password = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.userName
