e0759971fcb638fbc8d0f3a5cdc37774822ec5b4
irfiacre <irfiacre@gmail.com>
2024-10-29 22:24:49 +0200

    Adding models for Thread and Posts

--- threadflow/api/models.py
+++ threadflow/api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
-
 class User(models.Model):
     firstname=models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
@@ -15,32 +13,36 @@ class User(models.Model):
     def __str__(self) -> str:
         return self.username
 
-
-class Posts(models.Model):
-    message=models.CharField(max_length=1000)
-    owner_username = models.CharField(max_length=20)
-    created_at = models.CharField(max_length=50, default=datetime.now())
-    updated_at = models.CharField(max_length=100, null=True)
-
-    def __str__(self) -> str:
-        return self.message
-
-
-class Threads(models.Model):
+class Thread(models.Model):
     title=models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    owner_username = models.CharField(max_length=20)
     photo_url=models.CharField(max_length=500, null=True)
     created_at = models.CharField(max_length=50, default=datetime.now())
     updated_at = models.CharField(max_length=100, null=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        blank=False
+    )
 
     def __str__(self) -> str:
         return self.title
 
-
-class ThreadPosts(models.Model):
-    thread_id=models.IntegerField()
-    post_id = models.IntegerField()
+class Post(models.Model):
+    message=models.CharField(max_length=1000)
+    owner_username = models.CharField(max_length=20)
+    created_at = models.CharField(max_length=50, default=datetime.now())
+    updated_at = models.CharField(max_length=100, null=True)
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.PROTECT,
+        blank=False
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        blank=False
+    )
 
     def __str__(self) -> str:
-        return self.thread_id
+        return self.message
--- threadflow/api/serializer.py
+++ threadflow/api/serializer.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Post, Thread
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        fields = '__all__'
+
+class ThreadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Thread
         fields = '__all__'
